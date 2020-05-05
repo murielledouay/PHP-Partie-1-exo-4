0d0164f29e3c98fe03f1db785b3c7d82d879e240
murielledouay <guyard_murielle@orange.fr>
2020-05-05 17:06:31 +0200

    PHP Partie 1 exo 4

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+//déclaration de la variable name avec sa valeur Durand
+$name = 'Durand';
+?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<head>
+  <meta charset="utf-8"/>
+  <link rel="stylesheet" href="assets/css/style.css"/>
+  <title>Partie 1 exo 4</title>
+</head>
+
+<body>
+
+  <p>Bonjour
+    <?php
+    //j'affiche avec echo la variable name sur mon navigateur et dans la continuité du php je note ,comment vas-tu?
+    echo $name;
+    ?>, comment vas-tu ?
+  </p>
+
+
+</body>
+</html>
